234f19421f0146fa05602362847da2f29645d18a
miro <miroslav.gruevski@gmail.com>
2026-05-28 18:20:41 +0200

    fix cask download urls for v0.1.6

--- Casks/snapshift.rb
+++ Casks/snapshift.rb
@@ -1,14 +1,18 @@
 cask "snapshift" do
-  version "0.1.5"
+  version "0.1.6"
+  # Installer filenames still use 0.1.5 from the Tauri release build.
+  build = "0.1.5"
 
   on_arm do
-    url "https://github.com/miroslav-gruevski/SnapShift/releases/download/v#{version}/SnapShift_#{version}_aarch64.dmg"
-    sha256 :no_check
+    url "https://github.com/miroslav-gruevski/SnapShift/releases/download/v#{version}/SnapShift_#{build}_aarch64.dmg",
+        verified: "github.com/miroslav-gruevski/SnapShift/"
+    sha256 "88a8553aa30961361c8d3fe0305031fe310dfbbf3f9bfae098b49737fafa8da6"
   end
 
   on_intel do
-    url "https://github.com/miroslav-gruevski/SnapShift/releases/download/v#{version}/SnapShift_#{version}_x64.dmg"
-    sha256 :no_check
+    url "https://github.com/miroslav-gruevski/SnapShift/releases/download/v#{version}/SnapShift_#{build}_x64.dmg",
+        verified: "github.com/miroslav-gruevski/SnapShift/"
+    sha256 "f933a85f94736b9b2c10172806f91783d9a676f27093d28ea55bd2f0139dccc2"
   end
 
   name "SnapShift"
@@ -16,6 +20,10 @@ cask "snapshift" do
   homepage "https://github.com/miroslav-gruevski/SnapShift"
 
   app "SnapShift.app"
+
+  caveats <<~EOS
+    SnapShift is not notarized by Apple. On first launch, right-click SnapShift in Applications and choose Open.
+  EOS
 
   zap trash: [
     "~/Library/Application Support/com.miro.snapshift",
